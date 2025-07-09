48d0764696abe8c3903781d83ebcdc6e7aa5945d
Bridget F. <107891502+good-bean@users.noreply.github.com>
2025-07-08 21:14:29 -0700

    Update main.py
    Redoing game lesson

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
@@ -10,17 +7,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
-    
-    clock = pygame.time.Clock()
-    dt = 0
 
-    while True:
+    running = True
+
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(screen, (0,0,0))
+                running = False
+        screen.fill("black")
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
