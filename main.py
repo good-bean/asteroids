c69cc43f3b8b4fecd4960f887077b164b6e8be90
Bridget F. <107891502+good-bean@users.noreply.github.com>
2025-07-08 21:27:31 -0700

    Added clock
    Adding framerate

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from constants import *
 
 def main():
     pygame.init()
-    print('Starting asteroids!')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
-
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill("black")
+                
+        screen.fill('black')
         pygame.display.flip()
 
-if __name__ == "__main__":
+        dt = clock.tick(60) / 1000
+
+if __name__ == '__main__':
     main()
 
