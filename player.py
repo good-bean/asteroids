9a0a721a41d24e4251fda8ce5e60ae702fe33088
Bridget F. <107891502+good-bean@users.noreply.github.com>
2025-07-08 22:42:11 -0700

    Add rotation ability
    Add ability to rotate left and right using keys 'A' and 'D' on QWERTY keyboards

--- player.py
+++ player.py
@@ -22,3 +22,14 @@ class Player(CircleShape):
             'white',
             self.triangle(),
             2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
