f2308c75f5f672f057bd4d649926435e66823533
good-bean <bridget.s.flanery@gmail.com>
2025-09-14 21:45:42 -0700

    We have asteroids! (they don't collide yet)

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pygame
-from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     
-    clock = pygame.time.Clock()
-    dt = 0
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     
     Player.containers = (updatable, drawable)
-    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
     
     while True:
         for event in pygame.event.get():
