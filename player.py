b02dfa2565345e1d3a8fe08b947188b464eb6871
good-bean <bridget.s.flanery@gmail.com>
2025-09-16 10:08:29 -0700

    slight adjustments for readability

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -29,7 +30,6 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
